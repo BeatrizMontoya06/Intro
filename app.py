49c8bf7016cc89147fb0eca97ff0de63938bea0f
BeatrizMontoya06 <allputrp8@gmail.com>
2026-08-20 10:04:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Hola mi nombres es Bea")
+
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("Facilmente puedo realizar backend y frontend")
+image = Image.open('mono.jpg')
+st.image(image, caption='Interfaces multimodales')
